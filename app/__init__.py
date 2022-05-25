93b533e447799c4ebf27508d39925b81226f22dd
danielcatto <danielcatto@hotmail.com>
2022-05-25 15:12:18 -0300

    criado arquivo model tables.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+
 
 #import precisa ser declarado depois da variavel app ser declarado 
 from app.controllers import default
